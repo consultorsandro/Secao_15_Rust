a2a5a32eb6f6e94d70c3fcbcefbb52473d05a527
Sandro Reis <consultorsandro@hotmail.com>
2025-06-12 09:30:05 -0300

    Class 252 Review Strings

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() { //Class 252
+    //&str
+    //String
+
+    let pirate = "Bloodhook";
+    let sailer = String::from("Pirate");
+    let bad_guy = pirate.to_string();
+
+    println!("{} {} {}", pirate, sailer, bad_guy);
+
+
+
+}
